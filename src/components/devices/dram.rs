d9fc8128fb765f821839e92fbdb59d5d8b9b255b
bqrry4 <alexandru.panish@gmail.com>
2025-09-15 16:45:34 +0300

    fix: `dram` buffer allocation to heap to prevent a possible overflow of the stack

--- src/components/devices/dram.rs
+++ src/components/devices/dram.rs
@@ -1,13 +1,13 @@
 pub const DRAM_SIZE: u64 = 512 * 1024;
 
 pub struct Dram {
-    memory: [u64; DRAM_SIZE as usize],
+    memory: Vec<u8>,
 }
 
 impl Dram {
     pub fn new() -> Self {
         Self {
-            memory: [0; DRAM_SIZE as usize],
+            memory: vec![0; DRAM_SIZE as usize],
         }
     }
 }
