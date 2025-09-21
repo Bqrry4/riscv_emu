18ec13a0663e9613e7400e4a9b2e62de9cea89a5
bqrry4 <alexandru.panish@gmail.com>
2025-09-21 17:05:38 +0300

    refactor: handle branch instructions in a single match

--- src/instructions/branch.rs
+++ src/instructions/branch.rs
@@ -1,5 +1,3 @@
-use arbitrary_int::{i13, u5};
-
 use crate::{cpu::Cpu, instructions::types::BType};
 
 const BEQ: u8 = 0x0;
@@ -13,51 +11,20 @@ pub fn handle_branch(cpu: &mut Cpu, instr: u32) {
     let btype = BType::new_with_raw_value(instr);
     let (rs1, rs2, imm, funct3) = (btype.rs1(), btype.rs2(), btype.imm(), btype.funct3());
 
-    match funct3.value() {
-        BEQ => instr_beq(cpu, rs1, rs2, imm),
-        BNE => instr_bne(cpu, rs1, rs2, imm),
-        BLT => instr_blt(cpu, rs1, rs2, imm),
-        BGE => instr_bge(cpu, rs1, rs2, imm),
-        BLTU => instr_bltu(cpu, rs1, rs2, imm),
-        BGEU => instr_bgeu(cpu, rs1, rs2, imm),
-        _ => {}
-    }
-}
+    let lhs = cpu.x_regs.read(rs1);
+    let rhs = cpu.x_regs.read(rs2);
 
-/* 2.5.2. Conditional Branches */
-fn instr_beq(cpu: &mut Cpu, rs1: u5, rs2: u5, imm: i13) {
-    if cpu.x_regs.read(rs1).ne(&cpu.x_regs.read(rs2)) {
-        return;
+    /* 2.5.2. Conditional Branches */
+    let take_branch = match funct3.value() {
+        BEQ => lhs.eq(&rhs),
+        BNE => lhs.ne(&rhs),
+        BLT => (lhs as i64).lt(&(rhs as i64)),
+        BGE => (lhs as i64).ge(&(rhs as i64)),
+        BLTU => lhs.lt(&rhs),
+        BGEU => lhs.ge(&rhs),
+        _ => false,
+    };
+    if take_branch {
+        cpu.pc = cpu.pc.wrapping_sub(4).wrapping_add(imm.value() as u64);
     }
-    cpu.pc = cpu.pc.wrapping_sub(4).wrapping_add(imm.value() as u64);
-}
-fn instr_bne(cpu: &mut Cpu, rs1: u5, rs2: u5, imm: i13) {
-    if cpu.x_regs.read(rs1).eq(&cpu.x_regs.read(rs2)) {
-        return;
-    }
-    cpu.pc = cpu.pc.wrapping_sub(4).wrapping_add(imm.value() as u64);
-}
-fn instr_blt(cpu: &mut Cpu, rs1: u5, rs2: u5, imm: i13) {
-    if (cpu.x_regs.read(rs1) as i64).ge(&(cpu.x_regs.read(rs2) as i64)) {
-        return;
-    }
-    cpu.pc = cpu.pc.wrapping_sub(4).wrapping_add(imm.value() as u64);
-}
-fn instr_bltu(cpu: &mut Cpu, rs1: u5, rs2: u5, imm: i13) {
-    if cpu.x_regs.read(rs1).ge(&cpu.x_regs.read(rs2)) {
-        return;
-    }
-    cpu.pc = cpu.pc.wrapping_sub(4).wrapping_add(imm.value() as u64);
-}
-fn instr_bge(cpu: &mut Cpu, rs1: u5, rs2: u5, imm: i13) {
-    if (cpu.x_regs.read(rs1) as i64).lt(&(cpu.x_regs.read(rs2) as i64)) {
-        return;
-    }
-    cpu.pc = cpu.pc.wrapping_sub(4).wrapping_add(imm.value() as u64);
-}
-fn instr_bgeu(cpu: &mut Cpu, rs1: u5, rs2: u5, imm: i13) {
-    if cpu.x_regs.read(rs1).lt(&cpu.x_regs.read(rs2)) {
-        return;
-    }
-    cpu.pc = cpu.pc.wrapping_sub(4).wrapping_add(imm.value() as u64);
 }
