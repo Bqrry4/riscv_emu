42230b6bd14c6972a436f601b8e1ffe2322cc3af
bqrry4 <alexandru.panish@gmail.com>
2025-09-29 21:06:54 +0300

    build: dts via flake.nix

--- assets/flake.nix
+++ assets/flake.nix
@@ -15,43 +15,64 @@
       };
     in
     {
-      packages.${system}.opensbi = pkgs.stdenv.mkDerivation {
-        pname = "opensbi";
-        version = "1.7";
+      packages.${system} = {
+        opensbi = pkgs.stdenv.mkDerivation {
+          pname = "opensbi";
+          version = "1.7";
 
-        src = pkgs.fetchFromGitHub {
-          owner = "riscv-software-src";
-          repo = "opensbi";
-          rev = "v1.7";
-          sha256 = "0lp2j4cgnv9i1rhw8kdjfzqpzbrg4hvl1c6mxk1l3j612z2rkrjp";
+          src = pkgs.fetchFromGitHub {
+            owner = "riscv-software-src";
+            repo = "opensbi";
+            rev = "v1.7";
+            sha256 = "0lp2j4cgnv9i1rhw8kdjfzqpzbrg4hvl1c6mxk1l3j612z2rkrjp";
+          };
+
+          nativeBuildInputs = [
+            pkgs.buildPackages.gnumake
+            pkgs.buildPackages.python3
+          ];
+
+          makeFlags = [
+            "PLATFORM=generic"
+            "FW_DYNAMIC=y"
+            "PLATFORM_RISCV_XLEN=64"
+            "PLATFORM_RISCV_ISA=rv64ima_zicsr_zifencei"
+            "PLATFORM_RISCV_ABI=lp64"
+            "CROSS_COMPILE=${pkgs.stdenv.cc.targetPrefix}"
+          ];
+
+          # fix shebangs
+          postPatch = ''
+            scripts=$(find . -type f -name "*.py" -o -name "*.sh")
+            for s in $scripts; do
+              patchShebangs $s
+            done
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp build/platform/generic/firmware/fw_dynamic.bin $out/opensbi.bin
+          '';
         };
 
-        nativeBuildInputs = [
-          pkgs.buildPackages.gnumake
-          pkgs.buildPackages.python3
-        ];
+        dts = pkgs.stdenv.mkDerivation {
+          name = "dts";
 
-        makeFlags = [
-          "PLATFORM=generic"
-          "FW_DYNAMIC=y"
-          "PLATFORM_RISCV_XLEN=64"
-          "PLATFORM_RISCV_ISA=rv64ima_zicsr_zifencei"
-          "PLATFORM_RISCV_ABI=lp64"
-          "CROSS_COMPILE=${pkgs.stdenv.cc.targetPrefix}"
-        ];
+          src = ./.;
 
-        # fix shebangs
-        postPatch = ''
-          scripts=$(find . -type f -name "*.py" -o -name "*.sh")
-          for s in $scripts; do
-            patchShebangs $s
-          done
-        '';
+          nativeBuildInputs = [
+            pkgs.buildPackages.dtc
+          ];
+          buildPhase = ''
+            ls . -all
+            dtc virt.dts -I dts -O dtb -o virt.dtb
+          '';
 
-        installPhase = ''
-          mkdir -p $out
-          cp build/platform/generic/firmware/fw_dynamic.bin $out/opensbi.bin
-        '';
+          installPhase = ''
+            mkdir -p $out
+            mv virt.dtb $out/
+          '';
+        };
       };
     };
 }
